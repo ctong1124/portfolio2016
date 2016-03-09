e2860c639174261b72edc2e025510f71f60fbdf4
Carisa Tong <ctong1124@gmail.com>
2016-03-09 13:22:23 -0800

    black overlay for rest works with boolean in every object and for loop

--- app/work/work.js
+++ app/work/work.js
@@ -10,56 +10,86 @@ angular.module('myApp.work', ['ngRoute'])
 }])
 
 .controller('WorkCtrl', ['$scope', function($scope) {
+	
+    // $scope.hoverActive = false;
+  
 	$scope.tiles = [
+		{
+			name: 'Spot',
+			type: 'Web Development, UI',
+			cover: 'img/spot_iphone.jpg',
+			cover_size:'60em 40em, cover',
+			cover_pos: '80% 50%',
+			descrip: 'Lol kill me now',
+			show_title: false,
+			black_overlay: false
+		},
 		{
 			name: 'A Guide to House Music',
 			type: 'Web Development, UI',
 			cover: 'img/record.png',
 			cover_size:'90em 60em, cover',
-			cover_pos: '30% 50%',
+			cover_pos: '65% 50%',
 			descrip: 'Lol kill me now',
-			show_title: false
+			show_title: false,
+			black_overlay: false
 		},
-		{
-			name: 'Modelling the Zombie Apocalypse',
-			type: 'Web Development, UI',
-			cover: 'img/record.png',
-			cover_size:'90em 60em, cover',
-			cover_pos: '55% 50%',
-			descrip: 'Maybe i die',
-			show_title: false
-		},
-		{
-			name: 'Tastemakers Magazine',
-			type: 'Art Direction/Graphic Design',
-			cover: 'img/record.png',
-			cover_size:'90em 60em, cover',
-			cover_pos: '12% 70%',
-			descrip: 'Lol kill me now',
-			show_title: false
-		},
+		
 		{
 			name: 'Shutterstock',
 			type: 'Graphic Design',
-			cover: 'img/record.png',
-			cover_size:'90em 60em, cover',
-			cover_pos: '10% 90%',
+			cover: 'img/moo_cover.png',
+			// cover_size:'45em 30em, cover',
+			// cover_pos: '20% 70%',
+			cover_size:'60em 30em, cover',
+			cover_pos: '72% 70%',
 			descrip: 'Lol kill me now',
-			show_title: false
+			show_title: false,
+			black_overlay: false
 		},
+		
+		
+
 		{
-			name: 'Spot',
+			name: 'Modelling the Zombie Apocalypse',
 			type: 'Web Development, UI',
-			cover: 'img/record.png',
-			cover_size:'90em 60em, cover',
-			cover_pos: '2% 30%',
+			cover: 'img/ipad_apocalypse.jpg',
+			cover_size:'45em 29em, cover',
+			cover_pos: '40% 100%',
+			descrip: 'Maybe i die',
+			show_title: false,
+			black_overlay: false
+		},
+		
+		{
+			name: 'Tastemakers Magazine',
+			type: 'Art Direction/Graphic Design',
+			cover: 'img/tmm_mockup.jpg',
+			cover_size:'66em 44em, cover',
+			cover_pos: '50% 40%',
 			descrip: 'Lol kill me now',
-			show_title: false
-		}
+			show_title: false,
+			black_overlay: false
+		},
+		
+		
 	];
 
-	// $scope.hover = function(tile) {
-	//         // Shows/hides the delete button on hover
-	//         return tile.show_title = ! tile.show_title;
-	//     };
+
+
+	$scope.hover = function(tile) {
+	        // Shows/hides the delete button on hover
+	        // angular.forEach($scope.tiles, function(tile, key) {
+	        // 	tile.black_overlay =!tile.black_overlay;
+	        // });
+	        return tile.show_title = ! tile.show_title;
+	       
+	    };
+
+    // $scope.blackOverlayHuh = function(hoverActive) {
+    //     // Shows/hides the delete button on hover
+    //     return hoverActive;
+    // };
+
+
 }]);
