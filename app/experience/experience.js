150ac2ae1a9a189deec9e40ee4dce8beeea8c9e3
Carisa Tong <ctong1124@gmail.com>
2016-03-17 17:35:19 -0700

    experience/skills page setup

--- app/experience/experience.js
+++ app/experience/experience.js
@@ -17,8 +17,8 @@ angular.module('myApp.experience', ['ngRoute'])
 			dates: "Class of 2011", 
 			description: [
 			{
-				"Bachelor of Science": " in Mathematics with a minor in Computer Science",
-				"Awards": " cum laude, Dean's Scholarship, Honors Program"
+				"Academic GPA": " 3.8",
+				"Activities": " Design Editor for The Epitaph (school newspaper), Vice President of Forte Music Club, Wind Ensemble, Symphony Orchestra, Red Cross Club"
 			}]
 		},
 		{
@@ -28,7 +28,9 @@ angular.module('myApp.experience', ['ngRoute'])
 			description: [
 			{
 				"Bachelor of Science": " in Mathematics with a minor in Computer Science",
-				"Awards": " cum laude, Dean's Scholarship, Honors Program"
+				"Awards:": " cum laude, Dean's Scholarship, Honors Program",
+				"GPA:": " 3.517",
+				"Relevant coursework:": " Fundamentals of Computer Science I-II, Object-Oriented Design, Logic and Computation, Networks and Distributed Systems, Web Design and Development, Programming for Digital Media, Computer Graphics, Computer Systems, Probability and Statistics, Calculus II-III, Linear Algebra, Real Analysis"
 			}]
 		}
 	];
@@ -38,25 +40,47 @@ angular.module('myApp.experience', ['ngRoute'])
 			name: "Digital Analytics Co-op at Boston Globe Media",
 			location: "Boston, MA",
 			dates: "July 2013 - December 2013", 
-			description: ["This thing 1", "this thing 2"]
+			description: [
+			{
+				"Provided insight and analysis": " of data pertaining to user behavior of the Boston Globe Media websites using Adobe Analytics (formerly Omniture/SiteCataylst)",
+				"Worked on a small team": " to support ad hoc data requests from the organization", 
+				"Designed data visualizations" : " for analytical reports for Boston Globe Media"
+			}]
+			
 		},
 		{
 			name: "Art Director at Tastemakers Magazine",
 			location: "Northeastern University, Boston, MA",
 			dates: "May 2013 - December 2014", 
-			description: ["This thing 1", "this thing 2"]
+			description: [
+			{
+				"Oversaw all artistic aspects of Tastemakers Magazine,": "Northeastern University’s print and online publication focused on music journalism and criticism",
+				"Managed a team of designers": " and illustrators who created print magazine elements",
+				"Coordinated with": " writing team and professional printers to meet publishing deadlines", 
+				"Designed editorial spreads": " for magazine and event promo material, print and online"
+			}]
 		},
+		 
 		{
 			name: "Mathematics Tutor at Northeastern Univeristy",
 			location: "Boston, MA",
 			dates: "July 2014 - August 2014", 
-			description: ["This thing 1", "this thing 2"]
+			description: [
+			{
+				"Tutored for": " Northeastern’s Mathematics department classes including Mathematical Thinking, Calculus I-III, Differential Equations, Linear Algebra, Probability and Statistics",
+				"Counseled students": " of different levels of mathematical comprehension"
+			}]
 		},
 		{
 			name: "Graphic Design Intern at Shutterstock",
 			location: "New York, New York",
 			dates: "January 2015 - April 2015", 
-			description: ["This thing 1", "this thing 2"]
+			description: [
+			{
+				"Created design assets": " for marketing campaigns serving North America, South America, Asia and European markets",
+				"Designed promotional material": " for Shutterstock affiliated events such as Pixels of Fury and AIGA Portfolio Night alongside Shutterstock Art Directors and Events staff", 
+				"Made executive presentations" : " slide decks and informational material for internal clients to aid business growth and development"
+			}]
 		}
 	];
 
--- app/skills/skills.js
+++ app/skills/skills.js
@@ -9,6 +9,129 @@ angular.module('myApp.skills', ['ngRoute'])
   });
 }])
 
-.controller('SkillsCtrl', [function() {
+.controller('SkillsCtrl', ['$scope', function($scope) {
 
+	$scope.skills = [
+		{
+			name: "InDesign",
+			level: 95,
+			view_description: false,
+			description: [
+			"this is an example", "this is another example"]
+		},
+		{
+			name: "Photoshop",
+			level: 90,
+			view_description: false,
+			description: [
+			"this is an example", "this is another example"]
+		},
+		{
+			name: "Illustrator",
+			level: 75,
+			view_description: false,
+			description: [
+			"this is an example", "this is another example"]
+		},
+		{
+			name: "Java",
+			level: 75,
+			view_description: false,
+			description: [
+			"this is an example", "this is another example"]
+		},
+		{
+			name: "Javascript",
+			level: 70,
+			view_description: false,
+			description: [
+			"this is an example", "this is another example"]
+		},
+		{
+			name: "JQuery",
+			level: 70,
+			view_description: false,
+			description: [
+			"this is an example", "this is another example"]
+		},
+		{
+			name: "HTML/CSS",
+			level: 65,
+			description: [
+			"this is an example", "this is another example"]
+		},
+		{
+
+			name: "Python",
+			level: 60,
+			view_description: false,
+			description: [
+			"this is an example", "this is another example"]
+		},
+		{
+
+			name: "PHP",
+			level: 50,
+			view_description: false,
+			description: [
+			"this is an example", "this is another example"]
+		},
+		{
+
+			name: "AngularJS",
+			level: 30,
+			view_description: false,
+			description: [
+			"this is an example", "this is another example"]
+		},
+		{
+
+			name: "D3",
+			level: 25,
+			view_description: false,
+			description: [
+			"this is an example", "this is another example"]
+		}
+		
+	];
+
+	$scope.toggleDescription  = function(skill) {
+	        return skill.view_description = !skill.view_description;
+	       
+	    };
+
+
+	$scope.skillLevelString  = function(skill) {
+	        return {
+	        	"width": skill.level.toString().concat("%")
+	        };
+	       
+	    };
+
+
+
+	$scope.interests = [
+		{
+			name: "Music",
+			level: 95,
+			view_description: false,
+			description: [
+			"this is an example", "this is another example"]
+		},
+		{
+			name: "Food",
+			level: 90,
+			view_description: false,
+			description: [
+			"this is an example", "this is another example"]
+		},
+		{
+			name: "Tornados",
+			level: 85,
+			view_description: false,
+			description: [
+			"this is an example", "this is another example"]
+		}
+		
+	];
 }]);
