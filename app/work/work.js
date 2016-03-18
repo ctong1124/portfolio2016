8425f6af5815dbab7316d572e661410f4d43a345
Carisa Tong <ctong1124@gmail.com>
2016-03-18 15:12:44 -0700

    no message

--- app/work/work.js
+++ app/work/work.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var $location;
+
 
 angular.module('myApp.work', ['ngRoute'])
 
@@ -11,61 +11,9 @@ angular.module('myApp.work', ['ngRoute'])
   });
 }])
 
-.service('anchorSmoothScroll', function(){
-    
-    this.scrollTo = function(eID) {
 
-        // This scrolling function 
-        // is from http://www.itnewb.com/tutorial/Creating-the-Smooth-Scroll-Effect-with-JavaScript
-        
-        var startY = currentYPosition();
-        var stopY = elmYPosition(eID);
-        var distance = stopY > startY ? stopY - startY : startY - stopY;
-        if (distance < 100) {
-            scrollTo(0, stopY); return;
-        }
-        var speed = Math.round(distance / 100);
-        if (speed >= 20) speed = 20;
-        var step = Math.round(distance / 25);
-        var leapY = stopY > startY ? startY + step : startY - step;
-        var timer = 0;
-        if (stopY > startY) {
-            for ( var i=startY; i<stopY; i+=step ) {
-                setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
-                leapY += step; if (leapY > stopY) leapY = stopY; timer++;
-            } return;
-        }
-        for ( var i=startY; i>stopY; i-=step ) {
-            setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
-            leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
-        }
-        
-        function currentYPosition() {
-            // Firefox, Chrome, Opera, Safari
-            if (self.pageYOffset) return self.pageYOffset;
-            // Internet Explorer 6 - standards mode
-            if (document.documentElement && document.documentElement.scrollTop)
-                return document.documentElement.scrollTop;
-            // Internet Explorer 6, 7 and 8
-            if (document.body.scrollTop) return document.body.scrollTop;
-            return 0;
-        }
-        
-        function elmYPosition(eID) {
-            var elm = document.getElementById(eID);
-            var y = elm.offsetTop;
-            var node = elm;
-            while (node.offsetParent && node.offsetParent != document.body) {
-                node = node.offsetParent;
-                y += node.offsetTop;
-            } return y;
-        }
 
-    };
-    
-})
-
-.controller('WorkCtrl', ['$scope', '$location', function($scope, $location, anchorSmoothScroll) {
+.controller('WorkCtrl', ['$scope', function($scope) {
 	
     // $scope.hoverActive = false;
   
@@ -176,6 +124,8 @@ angular.module('myApp.work', ['ngRoute'])
 		tile.show_expanded = true;
 
 
+		
+		
 		return tile.show_expanded;
 
 			
@@ -183,18 +133,6 @@ angular.module('myApp.work', ['ngRoute'])
 
 	
 
-	$scope.gotoElement = function (eID){
-		// var eID = tile.idee;
-      // set the location.hash to the id of
-      // the element you wish to scroll to.
-      console.log(eID);
-      $location.hash('a');
- 		
- 		
-      // call $anchorScroll()
-      anchorSmoothScroll.scrollTo(eID);
-      
-    };
 
 
 	$scope.clickX = function(tile) {
@@ -273,6 +211,26 @@ angular.module('myApp.work', ['ngRoute'])
 
 
 }])
+
+.directive('scrollOnClick', function(){
+	return {
+	    restrict: 'A',
+	    link: function(scope, $elm) {
+	      $elm.on('click', function() {
+
+		      	// var scrolltop = 'scrollTop';
+		      	var scrollto = parseInt($(this).siblings(".expanded").offset().top);
+		      	console.log(scrollto);
+		      	// var scrollObject = {};
+		      	// scrollObject[scrolltop] = scrollto;
+		      	// console.log(scrollObject);
+		      	var scrollObject = {scrollTop: scrollto};
+		        $("body").delay("fast").animate(scrollObject, "slow");
+
+	      });
+	    }
+  	}
+})
 
 .animation('.slide-animation', function () {
         return {
